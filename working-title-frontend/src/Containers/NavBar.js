ecd9c5cec8d6643ffc0a8fce9a7c2f7c263b7348
SteRobWms <stephen.r.williams.87@gmail.com>
2020-05-26 15:08:25 -0500

    updated to master

--- working-title-frontend/src/Containers/NavBar.js
+++ working-title-frontend/src/Containers/NavBar.js
@@ -6,19 +6,6 @@ export default class NavBar extends React.Component {
 
     render() {
         return (
-<<<<<<< HEAD
-            <div className={"navBar"}>
-                <a href="http://localhost:3001/profile"><button className={"navBarButton"}>Profile</button></a>
-                <a href="http://localhost:3001/museums"><button className={"navBarButton"}>Museums</button></a>
-                <a href="http://localhost:3001/exhibits"><button className={"navBarButton"}>Exhibits</button></a>
-                {/* <a href="http://localhost:3001/items"><button className={"navBarButton"}>Items</button></a> */}
-                {/* <a href="http://localhost:3001/vrportal"><button className={"navBarButton"}>VR Portal</button></a> */}
-                <button className={"navBarButton"} onClick={() => {
-                    localStorage.clear()
-                    window.location.href = "http://localhost:3001/"
-                }}>Logout</button>
-            </div>
-=======
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
                 <div className="container">
                     <a className="navbar-brand" href="/">Muze 3D - Make your own VR Museums</a>
@@ -48,7 +35,6 @@ export default class NavBar extends React.Component {
                     </div>
                 </div>
             </nav>
->>>>>>> 39b4d5a3492ea4d292acec8d8632d83dc9be598e
         )
     }
 
